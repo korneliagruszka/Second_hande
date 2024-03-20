20bf42f19126b2fab8ac41dd2d96282880fae8ce
twój login do GitHuba <twj email>
2024-03-20 18:43:39 +0100

    1f_Home_page_Who_do_we_help_tbc

--- src/components/common/Contact/contact.jsx
+++ src/components/common/Contact/contact.jsx
@@ -3,7 +3,7 @@ import "../Contact/contact.scss";
 function Contact(){
     return(
         <div className="contact" id="contact">
-            <h1>Miejsce na formularz kontaktowy</h1>
+
         </div>
     )
 }
