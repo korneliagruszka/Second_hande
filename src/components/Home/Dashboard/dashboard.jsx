4a17a47216a932bfe1bb0e926d5f09146c8c9955
twój login do GitHuba <twj email>
2024-05-23 20:55:14 +0200

    Dashboard_tbc

--- src/components/Home/Dashboard/dashboard.jsx
+++ src/components/Home/Dashboard/dashboard.jsx
@@ -1,12 +1,19 @@
 import "./dashboard.scss";
 import Contact from "../../common/Contact/contact";
 import Form from "../../Home/Form/form";
+import bannerImage from "../../../assets/component.png";
+import secondImage from"../../../assets/Decoration.png";
 
 function Dashboard() {
   return (
     <>
     <div className="dashboard" id="dashboard">
-      <h2>Panel użytkownika</h2>
+      <img src={bannerImage} alt="Banner image" className="banner_image" />
+      <div className="banner_intro">
+        <h2>Oddaj rzeczy, których już nie chcesz POTRZEBUJĄCYM</h2>
+        <img src={secondImage} alt="Decoration image" className="banner_decoration_image"/>
+        <p>Wystarczą 4 proste kroki:</p>
+        </div>
     </div>
     <Form />
     <Contact />
