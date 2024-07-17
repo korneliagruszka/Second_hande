17ae401093b47f28730533b9e822e212b21219a4
twój login do GitHuba <twj email>
2024-07-17 19:49:11 +0200

    correctionss

--- src/env.js
+++
@@ -1,2 +0,0 @@
-export const VITE_SUPABASE_URL = 'https://bstwhfbdevvwtousteqz.supabase.co';
-export const VITE_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJzdHdoZmJkZXZ2d3RvdXN0ZXF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM4OTM0NjQsImV4cCI6MjAyOTQ2OTQ2NH0.Ph7pfZM9hlsuppT3XWKljZWxh4EXnDo6ujoQ3VDp3Gw';
