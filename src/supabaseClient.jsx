9e2341a7149f7354531d2cf62dbd5f20a1345614
twój login do GitHuba <twj email>
2025-03-29 19:19:20 +0100

    Corrections

--- src/supabaseClient.jsx
+++ src/supabaseClient.jsx
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import { VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY } from './env';
+import { VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY } from '../env.js';
 
 const supabaseUrl = VITE_SUPABASE_URL;
 const supabaseAnonKey = VITE_SUPABASE_ANON_KEY;
